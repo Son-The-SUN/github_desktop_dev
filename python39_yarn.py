a78facf8e820132d15b0f771ddae859e343c18fc
github_desktop_developer <GITHUB_DESKTOP_DEVELOPER@hosta.ai>
2024-06-02 01:22:56 -0400

    Update python39_yarn.py

--- python39_yarn.py
+++ python39_yarn.py
@@ -1,39 +1,25 @@
 import subprocess
-import webbrowser
-import socket
 import logging
 import os
-import signal
+
 curdir = os.path.dirname(os.path.realpath(__file__))
 logging.basicConfig(filename= f"{curdir}\\sonthesun_gh_desktop_log.txt",level=logging.DEBUG)
 logging.debug("I love you 3000")
 logger = logging.getLogger(f"{curdir}\\sonthesun_gh_desktop_log.txt")
 
-def check_port(port):
-  with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-      return s.connect_ex(('localhost', port)) == 0
-
-def kill_process_on_port(port):
-    command = f"netstat -ano | findstr :{port}"
-    result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
-    if result.returncode == 0:
-        for line in result.stdout.splitlines():
-            pid = line.split()[-1]
-            os.kill(int(pid), signal.SIGTERM)
-
 yarn_init = str(subprocess.check_output("yarn", shell=True))
 
-logger(yarn_init)
+logger.debug(yarn_init)
 
 yarn_build_dev = str(subprocess.check_output("yarn build:dev", shell=True))
 
-logger(yarn_build_dev)
+logger.debug(yarn_build_dev)
 
 yarn_start = str(subprocess.check_output("yarn start", shell=True))
 
-logger(yarn_start)
+logger.debug(yarn_start)
 
-logger("I love you 3000")
+logger.debug("I love you 3000")
 
 print("I love you 3000")
 
