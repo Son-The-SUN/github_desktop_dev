2728563fa1af8b31f7359a871e3b026ad4ac361f
github_desktop_developer <GITHUB_DESKTOP_DEVELOPER@hosta.ai>
2024-06-02 00:57:20 -0400

    Update python39_yarn.py

--- python39_yarn.py
+++ python39_yarn.py
@@ -20,6 +20,7 @@ def kill_process_on_port(port):
         for line in result.stdout.splitlines():
             pid = line.split()[-1]
             os.kill(int(pid), signal.SIGTERM)
+
 def start_I_love_you_3000():
   yarn_init = str(subprocess.check_output("yarn", shell=True))
 
@@ -27,22 +28,19 @@ def start_I_love_you_3000():
 
   yarn_build_dev = str(subprocess.check_output("yarn build:dev", shell=True))
 
-  print(yarn_build_dev)
+  logger(yarn_build_dev)
 
   yarn_start = str(subprocess.check_output("yarn start", shell=True))
 
-  print(yarn_start)
+  logger(yarn_start)
 
-  print("I love you 3000")
+  logger("I love you 3000")
 
 if __name__ == "__main__":
-  edge = webbrowser.get("edge")
   if check_port(3000):
     kill_process_on_port(3000)
-    start_I_love_you_3000()
-    edge.open("http://localhost:3000")
-  else:
-    raise Exception("Port 3000 is not available")
+  start_I_love_you_3000()
+
 
 
 
